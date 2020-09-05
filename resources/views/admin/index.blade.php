d8ac49b813ab668cd885dd9e311f3ab9f39fce6b
iyasmzn <iyasmzn07@gmail.com>
2020-09-05 10:31:19 +0700

    check 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
                     <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
                         <div class="website-traffic-ctn">
                             <h2><span class="counter">50,000</span></h2>
-                            <p>Total Website Traffics</p>
+                            <p>Totalitas Website Traffics</p>
                         </div>
                         <div class="sparkline-bar-stats1">9,4,8,6,5,6,4,8,3,5,9,5</div>
                     </div>
