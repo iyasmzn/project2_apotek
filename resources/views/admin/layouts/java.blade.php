28e733b4078029bd304a2a8d5549db47632a23d0
iyasmzn <iyasmzn07@gmail.com>
2020-09-23 14:17:46 +0700

    still crash

--- resources/views/admin/layouts/java.blade.php
+++ resources/views/admin/layouts/java.blade.php
@@ -36,21 +36,37 @@
         });
 
         // ChartJS
+        @php 
+            $drugs = DB::table('drugs')->get();
+            // dd($drugs);
+        @endphp
         var ctx = $('#myChart');
         var myChart = new Chart(ctx, {
             type: 'pie',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: [
+                    @foreach($drugs as $drug)
+                        '{{ $drug->name }}',
+                    @endforeach
+                ],
                 datasets: [{
                     label: '# of Votes',
-                    data: [99, 19, 3, 5, 2, 3],
+                    data: [
+                        @foreach($drugs as $drug)
+                            {{ $drug->stock }},
+                        @endforeach
+                    ],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -58,7 +74,10 @@
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
                         'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
                     ],
                     borderWidth: 1
                 }]
